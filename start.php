a8c713cd9da372de07846f651820679b2345fc9b
Naveen <naveen@ufl.edu>
2012-04-18 13:12:55 -0400

    used elggs input/captcha.php

--- start.php
+++ start.php
@@ -1,15 +1,18 @@
 <?php
 /**
  * recaptcha Plugin
- * 
+ * recaptcha settings are set in Elgg Admin Menu -> Settings -> recaptcha
+ * recaptcha library is included in /lib
+ * recaptcha form field is added in /mod/recaptcha/views/default/input/captcha.php
+ * @see Elggs default view /views/default/input/captcha.php
+ * @see registration form /views/default/forms/register.php [ echo elgg_view('input/captcha') ]
  */
 
 //register the plugin hook handler
 elgg_register_event_handler('init', 'system', 'recaptcha_init');
 
 /**
- * recaptcha settings are set in Elgg Admin Menu -> Settings -> recaptcha
- * recaptcha library is included in
+ * plugin init function
  */
 function recaptcha_init() {
 
@@ -21,9 +24,6 @@ function recaptcha_init() {
     $actions = __DIR__ . '/actions/recaptcha';
     elgg_register_action('recaptcha/settings/save', "$actions/settings/save.php", 'admin');
 
-    // include the recaptcha in the registration form
-    elgg_register_plugin_hook_handler('view', 'forms/register', 'add_recaptcha_to_form');
-
 	// register form check action when the user registration takes place
 	elgg_register_plugin_hook_handler('action', 'register', 'recaptcha_check_form');
 
@@ -31,53 +31,6 @@ function recaptcha_init() {
     elgg_register_plugin_hook_handler('register', 'user', 'recaptcha_unset_session');
 }
 
-
-/**
- * function to add recaptcha to the registration page
- * modifies the view /views/default/forms/register.php
- *
- * @param $hook
- * @param $type
- * @param $returnvalue
- * @param $params
- *
- * @return bool
- */
-function add_recaptcha_to_form($hook, $type, $returnvalue, $params) {
-
-    //add the recaptcha only if it has been enabled in the plugin setting
-    $plugin_entity = elgg_get_plugin_from_id('recaptcha');
-    $plugin_settings = $plugin_entity->getAllSettings();
-
-    if(array_key_exists('recaptcha_verified', $_SESSION) && $_SESSION['recaptcha_verified'] == 1) {
-
-        // no need for recaptcha again - user has already entered the correct value previously
-        $pattern = '<div class="elgg-foot">';
-        $recaptcha_option = "<label>".elgg_echo('recaptcha:label:human_verification')."</label><b>Verified</b><br><br>";
-        $replace = $pattern . $recaptcha_option;
-
-        $returnvalue = str_replace($pattern, $replace, $returnvalue);
-    }
-    else {
-        if($plugin_settings['require_recaptcha'] == 'on') {
-
-            // include the recaptcha lib
-            require_once('lib/recaptchalib.php');
-
-            $publickey = $plugin_settings['recaptcha_public_key'];
-            $recaptcha_option = "<label>".elgg_echo('recaptcha:label:human_verification')."</label><br>";
-            $recaptcha_option .= recaptcha_get_html($publickey);
-            $recaptcha_option .= '<br>';
-
-            $pattern = '<div class="elgg-foot">';
-            $replace = $pattern . $recaptcha_option;
-
-            $returnvalue = str_replace($pattern, $replace, $returnvalue);
-        }
-    }
-
-    return $returnvalue;
-}
 
 /**
  * @param $hook
